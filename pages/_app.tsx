ef6e5e9055aebe8c7ead45c2573971d2cd3eec81
Diego Carrillo <61395884+DiegoCarrillo32@users.noreply.github.com>
2022-11-05 23:14:34 -0600

    meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from '../components/navbar'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+    <Head>
+      
+    <title>Diego Carrillo Blog</title>
+        <meta charSet="UTF-8"/>
+        <meta name="keywords" content="Frontend, developer, backend, javascript, node, blog, development,"/>
+        <meta name="author" content="Diego Carrillo"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="I am a Computer engineer that focues on web solutions!"/>
+        <link rel="icon" href="./pixil-frame-0.png" />
+        <link rel="apple-touch-icon" href="./logo192.png" />
+        
+    </Head>
+        
       <Navbar/>
       <Component {...pageProps} />
     </>
