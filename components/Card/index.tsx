4b3716661f39d40e3057c625c7bb17b434bfb3ba
Diego Carrillo <61395884+DiegoCarrillo32@users.noreply.github.com>
2022-11-05 22:56:22 -0600

    create card and added navbar

---
+++ components/Card/index.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image';
+import React from 'react'
+import styles from './card.module.css';
+interface Card {
+    title: string;
+    description: string;
+    image: string;
+    link: string;
+    type:string;
+}
+export const Card = ( {title, description, image, link, type}:Card ) => {
+  return (
+    
+    
+        type === "small" ? (
+            <article className={styles["card-sm"]}>
+                <div className={styles["img-container-sm"]}>
+                    <Image src={image} alt={title}   layout='fill'  />
+                </div>
+                <h1>{title}</h1>
+                <p>{description}</p>
+                <a href={link}>Read more</a>
+            </article>
+        ) : (
+            <article className={styles["card-md"]}>
+                <div className={styles["img-container-md"]}>
+                    <Image src={image} alt={title}   layout='fill'  />
+                </div>
+                <div className={styles["text-md"]}>
+                    <h1>{title}</h1>
+                    <p>{description}</p>
+                </div>
+            </article>
+        )
+    
+  )
+}
---
+++ components/navbar/index.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from './navbar.module.css'
+import Link from 'next/link'
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = React.useState(false)
+    const handleOpen = () => {
+      setIsOpen(!isOpen)
+    }
+  return (
+    <>
+        <div className={isOpen ? `${styles['hamburger-active']}` : styles.hamburger} onClick={handleOpen}>
+            {
+                !isOpen ? 
+                (
+                    <>
+                        <div className={styles.line}></div>
+                        <div className={styles.line}></div>
+                        <div className={styles.line}></div>
+                    </>
+                ): 
+                (
+                <ul id='ul' className={styles.ul}>
+                    <li>
+                        <a href='#top'>
+                            <Image src={"/icon-512x512.png"} alt="My Logo" width={100} height={100} />
+                        </a>
+                    </li>
+                    
+                    <li>
+                        <Link href={"https://www.diegocarrillodev.com"}>My website</Link>
+                    </li>
+
+                <li>
+                    <a href='/CVDiegoC.pdf' style={{
+                        color:'#61D4F0',
+                    }}>CV</a>
+                
+                </li>
+            </ul>
+                )
+            }
+        </div>
+        <nav className={styles['nav-responsive']} id="navbar">
+            <div className={styles.space}>
+                    <a style={{
+                        paddingLeft:'50px',
+                        cursor:'pointer',
+                        marginTop:'-12px'
+                    }} href="#top"><Image src={"/icon-512x512.png"} alt="My Logo" width={100} height={100} /></a>
+            </div>
+            <ul id='ul'>
+                <li>
+                    <Link href={"https://www.diegocarrillodev.com"}>My website</Link>        
+                </li>
+                <li>
+                    <a href='/CVDiegoC.pdf' style={{
+                        color:'#61D4F0',
+                    }}>CV</a>
+                
+                </li>
+            </ul>
+        </nav>
+    </>
+  )
+}
+
+export default Navbar
